7218fcd77f58542a83dff2ac45e3b2ed0f3520e0
proxrustes <proxrustes@gmail.com>
2023-06-17 21:44:37 +0200

    DEMO

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,85 +1,83 @@
-"use client"
-
-import EventGrid from "@/components/EventGrid/EventGrid"
-import EventsModal from "@/components/EventModal"
-import BotMessage from "@/components/Messages/BotMessage"
-import UserMessage from "@/components/Messages/UserMessage"
-import { IChatMessage } from "@/definitions/interfaces/IChatMessage"
-import { Event } from "@/definitions/types/event"
-import { useState, useEffect, useRef, Fragment } from "react"
+'use client'
+import EventGrid from '@/components/EventGrid/EventGrid'
+import EventsModal from '@/components/EventModal'
+import BotMessage from '@/components/Messages/BotMessage'
+import UserMessage from '@/components/Messages/UserMessage'
+import { IChatMessage } from '@/definitions/interfaces/IChatMessage'
+import { Event } from '@/definitions/types/event'
+import { useState, useEffect, useRef, Fragment } from 'react'
 
 export default function Chatbot() {
   const [isOpen, setIsOpen] = useState(false)
-  const [input, setInput] = useState("")
+  const [query, setInput] = useState('')
+const [events, setEvent] = useState<Event[]|null>(null)
   const [history, setHistory] = useState<IChatMessage[]>([])
   const messagesEndRef = useRef<HTMLDivElement | null>(null)
-  const [events, setEvent] = useState<Event[] | null>(null)
-
   async function hasEvents(res) {
-    return false
+    return res.event
   }
-
   function closeModal() {
     setIsOpen(false)
   }
-
   function openModal() {
     setIsOpen(true)
   }
-
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value)
   }
-
   const handleSend = async () => {
-    if (input) {
+      setInput('')
+    if (query) {
       const userMessage: IChatMessage = {
-        message: input,
+        message: query,
         time: new Date().toISOString(),
-        user: "user"
+        user: 'user'
       }
-
       setHistory((prevHistory) => [...prevHistory, userMessage])
-
-      const res = await fetch('/api/chat', {
+      setTimeout(()=>{
+ const botMessage: IChatMessage = {
+        message: 'Hello, \n  Thank you for reaching out! We strive to offer the most memorable Icelandic experiences!\n Your answers will allow me to tailor a recommendation ensuring your Iceland trip is unforgettable. \n Let me figure out the perfect tour for you...',
+        time: new Date().toISOString(),
+        user:'bot'
+      }
+      setHistory((prevHistory) => [...prevHistory, botMessage])
+      }, 2000);
+      const res = await fetch('http://localhost:8000/api/chat', {
         headers: {
-          "Content-type": "application/json"
+          'Content-type': 'application/json'
         },
-        body: input,
-        method: "POST"
+        body: JSON.stringify({query}),
+        method: 'POST'
       })
-      if (await hasEvents(res)) {
-        const eventData = (await res.json())
-        setEvent(eventData)
+      const body = await res.json()
+      if (await hasEvents(body)) {
+        console.log(body)
+        setEvent(body.tours)
         openModal()
       } else {
         const botMessage: IChatMessage = {
-          message: "await res.json()",
+          message: 'await res.json()',
           time: new Date().toISOString(),
-          user: "bot"
+          user: 'bot'
         }
-
         setHistory((prevHistory) => [...prevHistory, botMessage])
       }
-      setInput("")
     }
   }
-
   useEffect(() => {
     if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
     }
   }, [history])
-
   return (
     <>
-      {" "}
-      <EventsModal isOpen={isOpen} closeModal={closeModal} events={events}/>
-      <div className="chat-wrapper px-5 flex flex-col justify-between">
-        <div className="px-[3vh]" style={{ overflowY: "auto" }}>
+      {''}
+      <EventsModal isOpen={isOpen} closeModal={closeModal} events={events} />
+      <div className='chat-wrapper px-5 flex flex-col justify-between'>
+        <div className='px-[3vh]' style={{ overflowY: 'auto' }}>
           {history.map((msg) => (
-            <div className="max-w-full">
-              {msg.user === "bot" ? (
+            <div className='max-w-full'>
+              {msg.user === 'bot' ? (
                 <BotMessage message={msg.message} />
               ) : (
                 <UserMessage message={msg.message} />
@@ -88,10 +86,10 @@ export default function Chatbot() {
           ))}
           <div ref={messagesEndRef} />
         </div>
-        <div style={{ marginTop: "auto" }}>
-          <div className="flex gap-x-[2vh]">
-            <input value={input} onChange={handleInputChange} />
-            <button type="button" onClick={handleSend}>Send</button>
+        <div style={{ marginTop: 'auto' }}>
+          <div className='flex gap-x-[2vh]'>
+            <input value={query} onChange={handleInputChange} />
+            <button type='button' onClick={handleSend}>Send</button>
           </div>
         </div>
       </div>
--- components/EventGrid/EventCard.tsx
+++ components/EventGrid/EventCard.tsx
@@ -1,9 +1,7 @@
 import { Event } from "@/definitions/types/event"
-
 interface Props {
   event: Event
 }
-
 export default function EventCard({ event }: Props) {
   return (
     <div
@@ -12,13 +10,13 @@ export default function EventCard({ event }: Props) {
     hover:scale-105 hover:shadow-lg h-[45vh] pt-[4vh]"
     >
       <div className="bg-waves bg-[#78A1BB] py-[4vh]">
-        <div className="bg-[#283044] py-[1vh]">
-          <a href={event.url} className="text-[3vh] font-black text-[#D9D9D9]">
+        <div className='bg-[#283044] py-[1vh]'>
+          <a href={event.url} className='text-[3vh] font-black text-[#D9D9D9]'>
             {event.title}
           </a>
         </div>
       </div>
-      <p className="text-[2vh] p-[2vh]">{event.description}</p>
+      <p className='text-[2vh] p-[2vh] h-[18vh] overflow-y-scroll'>{event.description}</p>
     </div>
   )
 }
