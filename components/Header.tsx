128b93fa9cf4a251ebacb6982927848a44558580
proxrustes <proxrustes@gmail.com>
2023-06-17 11:43:38 +0200

    + Header component

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <div className="text-lef mt-[1vh] px-[1vw]">
+        <p className="text-[4vh] italic font-black">AiT</p>
+        </div>
+    )
+}
